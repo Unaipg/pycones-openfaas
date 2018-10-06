b67197878e7a264e975372245305542f8ff8aae8
Javier Luna molina <javierlunamolina@gmail.com>
2018-10-06 10:13:12 +0200

    Reddit news

--- reddit-news/handler.py
+++ reddit-news/handler.py
@@ -1,10 +1,17 @@
 from reddit import get_reddit_posts
+
+def generate_html(posts):
+    HTML = "<head><title>NOTICIAS</title></head><body>"
+    for post in posts:
+        HTML += f"""<a href="{post['url']}><h3>{post['title']}</h3></a><br>"""
+    HTML += "</body>"
+    return HTML
+
 def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
     """
-    posts = get_reddit_posts()
-    news_posts = (x for x in posts if something)
-
-    return req
+    posts = [post for post in get_reddit_posts() if post['media'] is None]
+    resulting_html = generate_html(posts)
+    return resulting_html
--- template/python3/reddit.py
+++ template/python3/reddit.py
@@ -8,5 +8,5 @@ header = {"User-agent": "PyConEs demo KGB"}
 def get_reddit_posts():
     response = requests.get(os.getenv('SUBREDDIT_URL'), headers=header)
     if 199 > response > 299:
-        raise Exception("Reddit dont love us")
+        raise Exception("Reddit doesn't love us")
     return [x.get('data') for x in response.json().get('data').get('children')]
