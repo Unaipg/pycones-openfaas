43b376a1f2a50becaf0c5b1ee25e4c010d6a637a
Unai Pastor <unai.pastor@orbitalads.io>
2018-10-06 10:27:53 +0200

    This talk is being socialiced

--- reddit-news/handler.py
+++ reddit-news/handler.py
@@ -1,17 +1,17 @@
 from reddit import get_reddit_posts
 
-def generate_html(posts):
-    HTML = "<head><title>NOTICIAS</title></head><body>"
-    for post in posts:
-        HTML += f"""<a href="{post['url']}"><h3>{post['title']}</h3></a><br>"""
-    HTML += "</body>"
-    return HTML
 
 def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
     """
-    posts = [post for post in get_reddit_posts() if post['media'] is None]
-    resulting_html = generate_html(posts)
-    return resulting_html
+    posts = [post for post in get_reddit_posts() if not post['media']]
+    return generate_html(posts)
+
+def generate_html(posts):
+    HTML = "<head><title>NOTICIAS</title></head><body>"
+    for post in posts:
+        HTML += f"""<a href="{post['url']}"><h3>{post['title']}</h3></a><br>"""
+    HTML += "</body>"
+    return HTML
