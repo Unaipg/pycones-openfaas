dc44b703d4ed1f4fee8531685bce33cf6bc05c0b
Unai Pastor <unai.pastor@orbitalads.io>
2018-10-06 10:11:01 +0200

    Dank socialist memes

--- reddit-memes/handler.py
+++ reddit-memes/handler.py
@@ -1,9 +1,11 @@
-import requests
+from reddit import get_reddit_posts
 
 def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
     """
-    requests.get('http://google.com')
-    return req
+    posts = get_reddit_posts()
+    image_posts = [post for post in posts if post.get('url').endswith(('jpg', 'png', 'gif'))]
+    txt = '<br/><ln/>'.join(f"<h3>{post.get('title')}</h3><br><img src={post.get('url')}>" for post in image_posts)
+    return txt
--- template/python3/reddit.py
+++ template/python3/reddit.py
@@ -4,9 +4,9 @@ import requests
 from retrying import retry
 header = {"User-agent": "PyConEs demo KGB"}
 
-@retry()
+@retry(stop_max_attempt_number=10)
 def get_reddit_posts():
     response = requests.get(os.getenv('SUBREDDIT_URL'), headers=header)
-    if 199 > response > 299:
-        raise Exception("Reddit dont love us")
+    if 199 > response.status_code > 299:
+        raise Exception("Reddit is a dick")
     return [x.get('data') for x in response.json().get('data').get('children')]
